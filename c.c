a5eaabc663bf678a5ee7b0b086ed905cb6018559
JirongKim <munja__@naver.com>
2021-05-14 01:21:33 +0900

    first commit

---
+++ c.c
@@ -0,0 +1,25 @@
+//
+//  c.c
+//  test
+//
+//  Created by 김지현 on 2021/05/14.
+//
+
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    scanf("%d",&n);
+    
+    int odd = 0;
+    if(n%2==1){odd=1;n--;}
+    while (n) {
+        if(n%2==0){printf("1 ");}
+        else{printf("2 ");}
+        n--;
+    }
+    if(odd){printf("3");}
+    
+    return 0;
+}
