1067b8f15ac2953489df72ef4b2a0178170d5b87
JirongKim <munja__@naver.com>
2021-05-17 02:19:15 +0900

    Rename for list

---
+++ main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Baekjoon
+//
+//  Created by 김지현 on 2021/05/17.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
