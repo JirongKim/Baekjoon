d4e2571f6c5da7a655e6ca98ce7a6d9f8da44f82
JirongKim <munja__@naver.com>
2021-05-19 17:25:31 +0900

    Test Commit

--- 11000.c
+++ 11000.c
@@ -3,35 +3,37 @@
 int main(void)
 {
     int n;
-    scanf("%d" , &n);
+    scanf("%d", &n);
     
     int lecTime[200000];
-    for (int i =0; i<200000; i++) {
-        lecTime[i] =0;
+    for (int i = 0; i < 200000; i++) {
+        lecTime[i] = 0;
     }
     
-    int max =0 ;
+    int max = 0;
+    int min = 10e9 + 1;
     for (int i = 0; i < n; i++) {
         int s,t;
         scanf("%d %d", &s, &t);
-        if(lecTime[s] ==0 || lecTime[s] < t)
+        if(lecTime[s] == 0 || lecTime[s] < t)
             lecTime[s] = t;
-        if(lecTime[s]>max) max=lecTime[s];
+        if(lecTime[s] > max) max = lecTime[s];
+        if(lecTime[s] < min) min = lecTime[s];
     }
     
-    int now=1;
+    int now = 1;
     int cnt = 0;
     while (1) {
         int nowT = lecTime[now];
-        if(nowT==max){cnt++; break;}
+        if(nowT == max){ cnt++; break; }
         cnt++;
         
         int flag = 0;
-        for (int i = nowT; nowT>now; i--) {
-            if(lecTime[i]>lecTime[now])
+        for (int i = nowT; nowT > now; i--) {
+            if(lecTime[i] > lecTime[now])
             {
                 now = i;
-                flag=1;
+                flag = 1;
                 break;
             }
         }
@@ -39,7 +41,7 @@ int main(void)
         
         
         for (int i = nowT; ; i++) {
-            if(lecTime[i]>lecTime[now])
+            if(lecTime[i] > lecTime[now])
             {
                 now = i;
                 break;
@@ -47,9 +49,6 @@ int main(void)
         }
     }
     
-    printf("%d",cnt);
-    printf("%d",cnt);
-    printf("%d",cnt);
     printf("%d",cnt);
 }
 
